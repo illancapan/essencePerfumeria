2ef8c075b27502e0b175997aa6bf06d0b59aab7d
eibethdc <eibeth.delgado@gmail.com>
2024-08-31 18:19:41 -0400

    Agregue redes sociales al footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -16,9 +16,9 @@ function Footer() {
                             <button>Suscribete</button>
                         </div>
                         <div className={styles.socialIcons}>
-                            <span>FB</span>
-                            <span>TW</span>
-                            <span>IG</span>
+                            <a href={'https://web.facebook.com/?_rdc=1&_rdr'}><i className={'fa-brands fa-square-facebook fa-xl'}></i></a>
+                            <a href={'https://www.instagram.com/'}><i className={'fa-brands fa-instagram fa-xl'}></i></a>
+                            <a href={'https://x.com/'}><i className={'fa-brands fa-x-twitter fa-xl'}></i></a>
                         </div>
                     </div>
                     <div className={styles.footerBottom}>
