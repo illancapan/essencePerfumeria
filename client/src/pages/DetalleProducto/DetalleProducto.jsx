e45c469e552beb0614be3d0909c5ec430a87b9d4
illancapan <igorllancapan@gmail.com>
2024-09-14 20:12:43 -0300

    Guradado en los intentos

--- client/src/pages/DetalleProducto/DetalleProducto.jsx
+++ client/src/pages/DetalleProducto/DetalleProducto.jsx
@@ -1,45 +1,53 @@
-import { useContext} from 'react';
-import Footer from '../../components/footer/Footer';
-import Header from '../../components/header/Header';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import { useParams, Link } from 'react-router-dom';
+import { useContext } from 'react'
+import Footer from '../../components/footer/Footer'
+import Header from '../../components/header/Header'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import { useParams, Link } from 'react-router-dom'
 import { ProductoContext } from '../../context/ProductoContext'
 
 const DetalleProducto = () => {
     const { productos } = useContext(ProductoContext)
     const { id } = useParams()
 
-    const productosId = productos.filter((ele) => ele.id === id);
+    const productosId = productos.filter((ele) => ele.id === id)
 
     return (
         <>
             {/* Inicio del Banner */}
             <Header />
             {/* Fin del Banner */}
-            
             {/* Inicio del Contenido Principal */}
             <div className='w-75 m-auto p-5'>
-            {productosId.map((producto) =>(
-                <Card key={producto.id} >
-                    <div className="row g-0">
-                        <div className="col-md-4">
-                            <Card.Img style={{height: '100%'}} src={producto.imagen} />
-                        </div>
-                        <Card.Body className='col-md-8'>
-                            <Card.Title className='border-bottom pb-3'>{producto.nombre}</Card.Title>
-                            <Card.Text>
-                                {producto.descripcion}
-                            </Card.Text>
-                            <div className='d-flex justify-content-between'>
-                                <h4 className='p-3'>Precio: ${producto.precio}</h4>
-                                <Link to={'/carrito/'}><Button variant="danger" >Añadir 🛒</Button></Link>
+                {productosId.map((producto) => (
+                    <Card key={producto.id}>
+                        <div className='row g-0'>
+                            <div className='col-md-4'>
+                                <Card.Img
+                                    style={{ height: '100%' }}
+                                    src={producto.imagen}
+                                />
                             </div>
-                        </Card.Body>
-                    </div>               
-                </Card>
-            ))}
-        </div>  
+                            <Card.Body className='col-md-8'>
+                                <Card.Title className='border-bottom pb-3'>
+                                    {producto.nombre}
+                                </Card.Title>
+                                <Card.Text>{producto.descripcion}</Card.Text>
+                                <div className='d-flex justify-content-between'>
+                                    <h4 className='p-3'>
+                                        Precio: ${producto.precio}
+                                    </h4>
+                                    <Link to={'/carrito/'}>
+                                        <Button variant='danger'>
+                                            Añadir 🛒
+                                        </Button>
+                                    </Link>
+                                </div>
+                            </Card.Body>
+                        </div>
+                    </Card>
+                ))}
+            </div>
             {/* Fin del Contenido Principal */}
 
             {/* Inicio del Footer */}
